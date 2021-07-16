eb7c9311b0097b5c555c3811e17db5739bbdb7e1
sanghee-dev <leeesangheee@gmail.com>
2021-07-16 17:33:02 +0900

    struct SkinType

---
+++ Will I Burn/SkinType.swift
@@ -0,0 +1,12 @@
+struct SkinType {
+    let type1 = "Type 1 - Pale / Light"
+    let type2 = "Type 2 - White / Fair"
+    let type3 = "Type 3 - Medium"
+    let type4 = "Type 4 - Olive Brown"
+    let type5 = "Type 5 - Dark Brown"
+    let type6 = "Type 6 - Very Dark / Black"
+
+    func allSkinTypes() -> [String] {
+        return [type1, type2, type3, type4, type5, type6]
+    }
+}
