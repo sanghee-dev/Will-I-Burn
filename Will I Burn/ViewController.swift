775c5d158736c285fd3e782b019ccda681c38434
sanghee-dev <leeesangheee@gmail.com>
2021-07-16 17:12:19 +0900

    Initial Commit

---
+++ Will I Burn/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Will I Burn
+//
+//  Created by leeesangheee on 2021/07/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
