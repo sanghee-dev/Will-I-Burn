684782a6c95f36e5418bb6a1f51ebd841936c74c
sanghee-dev <leeesangheee@gmail.com>
2021-11-17 22:59:29 +0900

    스토리보드 오류 해결

---
+++ Will I Burn/Sources/Systems/SceneDelegate.swift
@@ -0,0 +1,18 @@
+//
+//  SceneDelegate.swift
+//  Will I Burn
+//
+//  Created by leeesangheee on 2021/07/16.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let _ = (scene as? UIWindowScene) else { return }
+    }
+}
+
