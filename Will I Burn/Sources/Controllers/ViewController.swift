7891841d023cac97f5449dd0267c628399ddbe60
sanghee-dev <leeesangheee@gmail.com>
2021-11-19 00:37:46 +0900

    UserNotification매니저 생성

--- Will I Burn/Sources/Controllers/ViewController.swift
+++ Will I Burn/Sources/Controllers/ViewController.swift
@@ -8,30 +8,30 @@
 import UIKit
 import CoreLocation
 
-class ViewController: UIViewController, CLLocationManagerDelegate {
+class ViewController: UIViewController {
     
     @IBOutlet weak var skinTypeLabel: UILabel!
     @IBOutlet weak var minutesLabel: UILabel!
     @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
     @IBOutlet weak var setReminderBtn: UIButton!
     
-    @IBAction func changeSkinBtnTap(_ sender: UIButton) { changeSkinBtnTap() }
-    @IBAction func setReminderBtnTap(_ sender: UIButton) { setReminderBtnTap() }
+    @IBAction func changeSkinBtnTap(_ sender: UIButton) { skinBtnTapped() }
+    @IBAction func setReminderBtnTap(_ sender: UIButton) { reminderBtnTapped() }
     
-    var uvIndex: Double = 10
     var burnTime: Int = 10
+    var coordinate: CLLocationCoordinate2D = .init() {
+        didSet { getWeatherData() }
+    }
+    var uvIndex: Double = 10 {
+        didSet { updateUI() }
+    }
     var skinType: SkinType = UserDefaultsManager.shared.getSkinType() {
-        didSet {
-            updateSkinLabel()
-            UserDefaultsManager.shared.setSkinType(skinType)
-        }
+        didSet { updateUI() }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         configureLocation()
-        updateSkinLabel()
     }
 }
 
@@ -43,75 +43,50 @@ extension ViewController {
     
     @objc func locationSuccess(_ notification: NSNotification) {
         if let coordinate = notification.userInfo?["coordinate"] as? CLLocationCoordinate2D {
-            getWeatherData(coordinate)
+            self.coordinate = coordinate
         }
     }
     
-    func getWeatherData(_ coordinate: CLLocationCoordinate2D) {
+    func getWeatherData() {
         WeatherManager.shared.getWeatherUV(coordinate) { result in
             switch result {
-            case .success(let uv):
-                self.uvIndex = uv
-                self.updateUI(dataSuccess: true)
-            case .failure(let error):
-                self.showAlert("Error", error.localizedDescription)
+            case .success(let uv): self.uvIndex = uv
+            case .failure(let error): self.showAlert("Error", error.localizedDescription)
             }
         }
     }
 }
 
 private extension ViewController {
-    func showAlert(_ title: String, _ message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        alert.addAction(.init(title: "OK", style: .default, handler: nil))
-        self.present(alert, animated: true, completion: nil)
-    }
-    
-    func updateUI(dataSuccess: Bool) {
-        if !dataSuccess {
-//            getWeatherData()
-            return
-        }
-        burnTime = Int(BurnTimeManager.shared.calcBurnTime(skinType, uvIndex))
-        minutesLabel.text = String(burnTime)
-        activityIndicator.stopAnimating()
-        setReminderBtn.isEnabled = true
-    }
-    
-    func changeSkinBtnTap() {
+    func skinBtnTapped() {
         let alert = UIAlertController(title: "Pink one", message: "Please choose your skin type", preferredStyle: .actionSheet)
         for skinType in SkinType.allCases {
             alert.addAction(UIAlertAction(title: skinType.rawValue, style: .default, handler: { [weak self] _ in
                 self?.skinType = skinType
-                self?.updateSkinLabel()
-                self?.updateUI(dataSuccess: true)
+                UserDefaultsManager.shared.setSkinType(skinType)
             }))
         }
         self.present(alert, animated: true, completion: nil)
     }
     
-    func setReminderBtnTap() {
-        let center = UNUserNotificationCenter.current()
-        center.removeAllPendingNotificationRequests()
-        
-        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
-            if !granted {
-                return
-            }
-            let content = UNMutableNotificationContent()
-            content.title = "Time's Up!"
-            content.body = "You are beginning to BURN! Please get in to the shade or use strong sunblock and cover up!"
-            content.sound = UNNotificationSound.default
-            
-            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(self.burnTime * 60), repeats: false)
-            let request = UNNotificationRequest(identifier: "burnNotification", content: content, trigger: trigger)
-            center.add(request, withCompletionHandler: nil)
-        }
-        
+    func reminderBtnTapped() {
+        UserNotificationMananger.shared.requestNotification(after: burnTime)
         showAlert("Reminder", "We will remind you after \(burnTime) minutes!")
     }
-    
-    func updateSkinLabel() {
+}
+
+private extension ViewController {
+    func updateUI() {
         skinTypeLabel.text = skinType.rawValue
+        burnTime = Int(BurnTimeManager.shared.calcBurnTime(skinType, uvIndex))
+        minutesLabel.text = String(burnTime)
+        setReminderBtn.isEnabled = true
+        activityIndicator.stopAnimating()
+    }
+    
+    func showAlert(_ title: String, _ message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(.init(title: "OK", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
     }
 }
---
+++ Will I Burn/Sources/Utilities/UserNotificationMananger.swift
@@ -0,0 +1,33 @@
+//
+//  UserNotificationMananger.swift
+//  Will I Burn
+//
+//  Created by leeesangheee on 2021/11/18.
+//
+
+import Foundation
+import UserNotifications
+
+class UserNotificationMananger {
+    static let shared = UserNotificationMananger()
+    let center = UNUserNotificationCenter.current()
+    
+    private init() {}
+    
+    func requestNotification(after time: Int) {
+        center.removeAllPendingNotificationRequests()
+        
+        center.requestAuthorization(options: [.alert, .badge, .sound]) { (granted, error) in
+            guard granted else { return }
+            
+            let content = UNMutableNotificationContent()
+            content.title = "Time's Up!"
+            content.body = "You are beginning to BURN!🥵"
+            content.sound = UNNotificationSound.default
+            
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: TimeInterval(time * 60), repeats: false)
+            let request = UNNotificationRequest(identifier: "burnNotification", content: content, trigger: trigger)
+            self.center.add(request, withCompletionHandler: nil)
+        }
+    }
+}
